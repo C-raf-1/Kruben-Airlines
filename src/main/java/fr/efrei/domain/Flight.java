8cf34844f2b78336a9f791d9487b22ef9f506870
Yoannbnrd <yoannb034@gmail.com>
2024-11-25 17:14:17 +0200

    Create Flight.java
    class flight

---
+++ src/main/java/fr/efrei/domain/Flight.java
@@ -0,0 +1,103 @@
+package fr.efrei.domain;
+
+public class Flight {
+    String idFlight;
+    String destination;
+    String departure;
+    String idPlane;
+    int price;
+    String date;
+
+
+    private Flight(FlightBuilder builder) {
+        this.idFlight = builder.idFlight;
+        this.destination = builder.destination;
+        this.departure = builder.departure;
+        this.idPlane = builder.idPlane;
+        this.price = builder.price;
+        this.date = builder.date;
+    }
+
+
+    public String getIdFlight() {
+        return idFlight;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public String getIdPlane() {
+        return idPlane;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight :\n"
+                +"=========================================================================\n"+
+                "➜ Flight number : " + idFlight + "\n" +
+                "➜ destination : " + destination + "\n" +
+                "➜ departure : " + departure + "\n" +
+                "➜ Plane number : " + idPlane + "\n" +
+                "➜ price : " + price + "\n" +
+                "➜ date : " + date + "\n" +
+                "===========================================================================\n"
+                +" ";
+    }
+
+    public static class FlightBuilder {
+        String idFlight;
+        String destination;
+        String departure;
+        String idPlane;
+        int price;
+        String date;
+
+        public FlightBuilder setIdFlight(String idFlight) {
+            this.idFlight = idFlight;
+            return this;
+        }
+
+        public FlightBuilder setDestination(String destination) {
+            this.destination = destination;
+            return this;
+        }
+
+        public FlightBuilder setDeparture(String departure) {
+            this.departure = departure;
+            return this;
+        }
+
+        public FlightBuilder setIdPlane(String idPlane) {
+            this.idPlane = idPlane;
+            return this;
+        }
+
+        public FlightBuilder setPrice(int price) {
+            this.price = price;
+            return this;
+        }
+
+        public FlightBuilder setDate(String date) {
+            this.date = date;
+            return this;
+        }
+
+
+        public Flight build() {
+            return new Flight(this);
+        }
+    }
+}
