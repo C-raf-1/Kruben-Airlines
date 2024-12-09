377fee722d3d32d4e42eab087ebd366f7145d66b
C-raf-1 <125271538+C-raf-1@users.noreply.github.com>
2024-12-09 20:51:30 +0100

    Update main
    menu

--- src/main/java/fr/efrei/Main.java
+++ src/main/java/fr/efrei/Main.java
@@ -41,6 +41,62 @@ public class Main {
         flight1 = repositoryF.create(flight1);
         flight2 = repositoryF.create(flight2);
         flight3 = repositoryF.create(flight3);
+        while (true) {//debut du menu
+            System.out.println("\n" + "type 1 to books a flight \n" + "type 2 to see your booking \n" + "type 3 to see the seats of our 3 planes \n" + "type 4 to manage your information");
+            String choice = scanner.nextLine();
+            String seaty = null;
+            int seatx = 0;
+            int isGood = 0;
+            switch (choice) {
+                case "1": //cas pour choisir les vols
+                    System.out.println(flight1+"\n"+flight2+"\n"+flight3+"\n");
+                    String flightChoice;
+
+                    while(true) {
+                        System.out.println("choose your flight or tap 4 to go back ");
+                        flightChoice = scanner.nextLine();
+                        boolean exists = false;
+
+                        if(flightChoice.equals("4")){
+                            break;
+                        }
+
+                        if (alreadyType.contains(flightChoice)) {
+                            System.out.println("Value already exists! Please enter a different value.");
+                        } else {
+                            // Ajouter le vol à la liste si ce n'est pas un doublon
+                            alreadyType.add(flightChoice);
+
+                            exists = true;
+                        }
+                        if (exists){
+                            break;
+                        }
+                    }//on verifie que le user ne puisse pas reserver 2 fois le meme vol
+                    if (flightChoice.equals("1")||flightChoice.equals("2")||flightChoice.equals("3")){
+                        Reservation reservation1;
+                        reservation1 = BookingFlightAndCreateReservation(plane1,plane2,plane3,flight1,flight2,flight3,customer1,flightChoice);
+                        repositoryR.create(reservation1);//on ajoute la reservation a la ligne.
+                        System.out.println("your reservation number is : " + reservation1.getIdBooking());
+                    }
+                    break;
+
+                case "2":
+                    reservationList = repositoryR.getall();
+                    alreadyType = menu2(reservationList,isGood,seaty,seatx,alreadyType);
+                    break;
+
+                case "3":
+                    menu3(plane1,plane2,plane3);
+                    break;
+
+                case "4":
+                    customer1 = menu4(customer1);
+                    break;
+                default:
+                    System.out.println("invalid input");
+            }
+        }
     }
 
 }
